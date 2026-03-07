26b3823ab4b6df4a40922f894526e6d7a30a1a96
Abhiram <abhirammokkala@gmail.com>
2026-03-07 14:19:19 +0530

    first commit

---
+++ OopsBannerUC4.java
@@ -0,0 +1,19 @@
+public class OopsBannerUC4 {
+
+    public static void main(String[] args) {
+
+
+        String[] banner = {
+                "O     O  OOOOO  PPPP   SSSS",
+                "O     O  O   O  P   P  S",
+                "O     O  O   O  PPPP   SSS",
+                "O     O  O   O  P         S",
+                " OOOOO   OOOOO  P      SSSS"
+        };
+
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
