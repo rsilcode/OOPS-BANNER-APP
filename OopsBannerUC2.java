ed2d16308090f21615ebec3bec37340d11b7f7ea
rsilcode <rsilcode@github.com>
2026-03-07 14:45:18 +0530

    3rd commit

--- OopsBannerUC2.java
+++ OopsBannerUC2.java
@@ -1,29 +1,12 @@
 public class OopsBannerUC2 {
 
     public static void main(String[] args) {
-        // UC2: Display OOPS in large ASCII art banner using * and spaces
-        // Using individual print statements with manual string concatenation
-        // 7-line ASCII art representation of "OOPS"
-        
-        // Line 1: Top border
         System.out.println("*     *  *****  *     *  *     *");
-        
-        // Line 2: First row of letters
         System.out.println("*     *  *   *  *     *  *     *");
-        
-        // Line 3: Second row
         System.out.println("*     *  *   *  *  *  *  *     *");
-        
-        // Line 4: Middle section - widest part
         System.out.println("*     *  *****  *  *  *  *  *  *");
-        
-        // Line 5: Fourth row
         System.out.println("*     *  *      * *   *  * * * *");
-        
-        // Line 6: Fifth row
         System.out.println("*     *  *      **    *  * * * *");
-        
-        // Line 7: Bottom border
         System.out.println(" *   *   *****  *     ****  * * * *");
     }
 }
--- OopsBannerUC3.java
+++ OopsBannerUC3.java
@@ -1,10 +1,6 @@
 public class OopsBannerUC3 {
 
     public static void main(String[] args) {
-        // Using String.join() method to build each line of the OOPS banner
-        // This approach is more memory efficient than concatenating with + operator
-        
-        // Define each letter's segments (rows)
         String[] oLine1 = {"O     O"};
         String[] oLine2 = {"O     O"};
         String[] oLine3 = {"O     O"};
@@ -23,7 +19,6 @@ public class OopsBannerUC3 {
         String[] sLine4 = {"     S"};
         String[] sLine5 = {" SSSS"};
         
-        // Array of O's segments
         String[] oSegments = {
             "OOOOO",
             "O   O",
@@ -32,14 +27,12 @@ public class OopsBannerUC3 {
             "OOOOO"
         };
         
-        // Build each line using String.join()
         String line1 = String.join("  ", "O     O", "OOOOO", "PPPP ", " SSSS");
         String line2 = String.join("  ", "O     O", "O   O", "P   P", "S    ");
         String line3 = String.join("  ", "O     O", "O   O", "PPPP ", "  SSS");
         String line4 = String.join("  ", "O     O", "O   O", "P    ", "     S");
         String line5 = String.join("  ", " OOOOO ", "OOOOO", "P    ", " SSSS");
         
-        // Print the banner
         System.out.println(line1);
         System.out.println(line2);
         System.out.println(line3);
